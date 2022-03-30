2e499644ed251e4b7e519ca98c561944f6096940
Ileenfdz <ileenfdz@gmail.com>
2022-03-30 20:36:22 +0200

    Mini example

---
+++ index.js
@@ -0,0 +1,18 @@
+// export function index(){
+//     console.log('Holi')
+
+//     new Promise((resolve, reject) =>{
+
+//         console.log('Cuerpo de la promesa')
+//         resolve('Promesa resuelta')
+//         reject('Promesa no resuelta')
+
+        //En el then solo saldría si se lleva a cabo el resolve, lo mismo con el reject y el catch
+//     }).then(console.log)
+//     .catch(console.log)
+
+//     console.log('Adiós')
+// }
+
+
+index()
